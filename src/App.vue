f6968aa6cea9f13ac506a67ef0a51fc8d876ffa4
rLaurentePn2k19 <rangie.laurente@student.passerellesnumeriques.org>
2019-09-11 20:36:18 -0700

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js Appas"/>
   </div>
 </template>
 
@@ -14,7 +14,7 @@ export default {
     HelloWorld
   }
 }
-</script>
+</script>u
 
 <style>
 #app {
