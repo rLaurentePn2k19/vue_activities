ffc91c2f6a59dfe1c53183418cf62dffcfce8dad
rLaurentePn2k19 <rangie.laurente@student.passerellesnumeriques.org>
2019-09-16 22:33:55 -0700

    initial

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VModal from 'vue-js-modal'
 import VueSweetalert2 from 'vue-sweetalert2'
+import { mdbContainer, mdbRow, mdbCol, mdbInput, mdbTextarea, mdbBtn, mdbIcon, mdbModal, mdbModalHeader, mdbModalBody, mdbModalFooter } from 'mdbvue'
+
+Vue.use({ mdbContainer, mdbRow, mdbCol, mdbInput, mdbTextarea, mdbBtn, mdbIcon, mdbModal, mdbModalHeader, mdbModalBody, mdbModalFooter })
 
 Vue.use(VModal)
 Vue.use(VueSweetalert2)
